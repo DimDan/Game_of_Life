38e745f6d28732628caac4ca0553ba2e6e636a8e
Dmitry Boyko <dimdan.ds@gmail.com>
2013-04-06 17:48:41 +1100

    Quick fix

--- misc/array_test_by_Danil.pas
+++ misc/array_test_by_Danil.pas
@@ -10,14 +10,8 @@ for j:=1 to 75 do begin
 A[i,j]:=random(4);
 if (A[i,j]=3) or (A[i,j]=2) then B[i,j]:=TRUE else B[i,j]:=FALSE;
 if (j=1) then write('|');
-if (B[i,j]=true) then write('*') else write('#'); 
+if (B[i,j]=true) then write('*') else write(' '); 
 if (j=75) then  write('|');
 if(j=75) then writeln;
 end;
-
-readln;
-///Тут начинается проверка)
-for i:=1 to 75 do
-for j:=1 to 75 do begin 
-if B[i,j]  // не написал еще
 end.
