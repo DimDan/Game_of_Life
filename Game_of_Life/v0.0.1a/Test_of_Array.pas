8b85276d9bd678bfe1497c86261f1e2a2acb20b5
Leon Liberty <dimdan.ds@gmail.com>
2013-04-06 17:00:25 +1100

    New version! (just a test)

---
+++ Game_of_Life/v0.0.1a/Test_of_Array.pas
@@ -0,0 +1,39 @@
+Program TEST;
+uses crt;
+Type
+  Matrix = array [1..3, 1..3] of integer;
+Var
+  field : matrix;
+//
+Procedure input (var f : matrix);
+  var i, j : integer;
+  begin
+    for i := 1 to 3 do
+      for j := 1 to 3 do readln (f[i][j]);
+  end;
+Procedure output (f : matrix);
+  var i, j : integer;
+      breaker : boolean;
+  begin
+    breaker := false;
+    while (breaker <> true) do begin
+      for i := 1 to 3 do
+        for j := 1 to 3 do begin
+          write (f[j][i]);
+          if (j = 3) then writeln();
+          if ((i = 3) and (j = 3)) then breaker := true;
+        end;
+    end;
+  end;
+//
+begin
+  writeln('Enter nine numbers in the array:');
+  input (field);
+  writeln ('You have succesfully entered all the numbers in the array! Press "Enter" to continue.');
+  readln();
+  clrscr;
+  output (field);
+  readln();
+end.
+  
+  
